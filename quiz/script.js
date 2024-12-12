9667faa1c317040887b2ac5ce395d1903eebe152
Georg Graf (SPG) <grafg@spengergasse.at>
2024-12-12 20:38:44 +0100

    autocommit

--- quiz/script.js
+++ quiz/script.js
@@ -1,11 +1,32 @@
-a = {
-    alter: 23,
-    name: "Tom",
-};
-b = {
-    alter: 17,
-    name: "Maria",
-};
+class Feuer {
+    temperatur;
+    helligkeit;
+    farbe;
+    gewicht;
+    groesse;
+    istAn;
+    constructor(
+        neuTemperatur,
+        neuHelligkeit,
+        neuFarbe,
+        neuGewicht,
+        neuGroesse,
+        neuistAn
+    ) {
+        this.temperatur = neuTemperatur;
+        this.helligkeit = neuHelligkeit;
+        this.farbe = neuFarbe;
+        this.gewicht = neuGewicht;
+        this.groesse = neuGroesse;
+        this.istAn = neuistAn;
+    }
+    toString() {
+        return `Dieses Feuer hat ${this.temperatur} Grad und leuchtet ${this.farbe}`;
+    }
+}
 
-c = JSON.stringify(new Date());
-console.log(c);
+const silvesterFeuer = new Feuer(1200, 300000, 'hellgelb', 300, 2.5, true);
+const kaminFeuer = new Feuer(800, 50000, 'orange', 4, 0.3, true);
+
+console.log(silvesterFeuer.toString());
+console.log(kaminFeuer.toString());
